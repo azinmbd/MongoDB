5430e09ab49b9f74ba6c41dc571dafd43dd998f0
Azin <48130819+azinmbd@users.noreply.github.com>
2023-03-30 21:29:45 -0700

    main

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 
-app.listen(7000);
-console.log("App running http://localhost:7000");
+app = express();
+
+const PORT = process.env.PORT || 7000;
+
+mongoose.connect(
+  "mongodb+srv:// tempuser:123@cluster0.f9d6o.gcp.mongodb.net/Exam"
+);
+
+const QuizSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  Sid: {
+    type: String,
+    required: true,
+  },
+});
+
+const QuizModel = mongoose.model("Quiz", QuizSchema);
 
 app.get("/", (req, res) => {
-  res.send(` <h1>name: "azin"</h1>
-  <h1>sid: 300365490</h1>`);
+  const azin = QuizModel({
+    name: "Azin Mobehdmehdiabadi",
+    Sid: "300365490",
+  });
+
+  QuizModel.insertMany(azin);
+
+  res.send(`<h1> name : "Azin Mobehdmehdiabadi </h1>
+    <h1> Sid : "300365490" </h1>
+    `);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running " + PORT);
 });
